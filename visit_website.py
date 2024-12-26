3743cadede0ef93c5d49902b3be6c26b875b6f03
haotiandao <haotianwuji@hotmail.com>
2024-12-26 16:03:47 +0800

    Update visit_website.py

--- visit_website.py
+++ visit_website.py
@@ -8,7 +8,7 @@ def visit_website(url):
         print(f'访问 {url} 失败: {str(e)}')
 
 if __name__ == '__main__':
-    visit_website('http://haotiandao.github.io')
+    visit_website('http://th.ip-ddns.com')
 
 
 
